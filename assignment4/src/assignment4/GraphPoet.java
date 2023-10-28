fc1550bf14308b4ec481fcd91e82bbb447a98872
JJ <jinjunoh2@gmail.com>
2023-10-28 16:15:11 -0500

    assignment4 init

---
+++ assignment4/src/assignment4/GraphPoet.java
@@ -0,0 +1,40 @@
+
+package assignment4;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class GraphPoet {
+
+
+    /**
+     *
+     * @param corpus text file from which to derive the poet's affinity graph
+     * @throws IOException if the corpus file cannot be found or read
+     */
+
+    public GraphPoet(File corpus) throws IOException {
+
+        /* Read in the File and place into graph here */
+    }
+
+    /**
+     * Generate a poem.
+     *
+     * @param input File from which to create the poem
+     * @return poem (as described above)
+     */
+    public String poem(File input) throws IOException {
+
+        /* Read in input and use graph to complete poem */
+        String poem = "";
+        return poem;
+    }
+
+}
---
+++ assignment4/src/assignment4/Main.java
@@ -0,0 +1,21 @@
+package assignment4;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    /**
+     * Example program using GraphPoet.
+     */
+
+    /**
+     * Generate example poetry.
+     *
+     * @param args unused
+     * @throws IOException if a poet corpus file cannot be found or read
+     */
+    public static void main(String[] args) throws IOException {
+        final GraphPoet nimoy = new GraphPoet(new File("corpus.txt"));
+        System.out.println(nimoy.poem(new File("input.txt")));
+    }
+}
